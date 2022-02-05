22bb3442fd82996bc45121f253df93fc10ed56f6
Rajan Agarwal <rajan.ag005@gmail.com>
2022-02-05 14:59:29 -0500

    move to ethereum mainnet

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from 'next/link';
 
-export default function Home({ graph }) {
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
