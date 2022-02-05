85a37514d355080c89631ece07f8ae8e0e1c6332
Rajan Agarwal <rajan.ag005@gmail.com>
2022-02-05 08:03:22 -0500

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_ID}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
