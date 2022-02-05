604ef128f3bed621e3a610d02abdfed1c47414d8
Rajan Agarwal <rajan.ag005@gmail.com>
2022-02-05 12:06:48 -0500

    ADD GRAPHICAL ANALYSIS IT WORKED!!

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,74 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const secure = require("crypto-js/sha256");
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+class Hash {
+	constructor(index, time, data, prev = '') {
+		this.index = index;
+		this.time = time;
+		this.data = data;
+		this.prev = prev;
+		this.hash = this.calculate();
+		this.nonce = 0;
+	}
+
+	calculate() {
+		return secure(this.index + this.prev + this.time + JSON.stringify(this.data) + this.nonce).toString();
+	}
+
+	mining(difficulty) {
+		while (
+			this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+		) {
+			this.nonce++;
+			this.hash = this.calculate();
+		}
+	}
 }
+
+class Protocol {
+	constructor() {
+		this.protocol = [this.genesis()];
+		this.difficulty = 4;
+	}
+
+	genesis() {
+		return new Hash(0, "01/01/2020", "you alr know its ya boy genesis on chain", "0");
+	}
+
+	fetch() {
+		return this.protocol[this.protocol.length - 1];
+	}
+
+	add(input) {
+		input.prev = this.fetch().hash;
+		input.hash = input.calculate();
+		input.mining(this.difficulty);
+		this.protocol.push(input);
+	}
+
+	validity() {
+		for (let i = 1; i < this.protocol.length; i++) {
+			const current = this.protocol[i];
+			const last = this.protocol[i - 1];
+
+			if (current.hash !== current.calculate()) {
+				return 'Not Mined. Current hash is invalid';
+			} else if (current.prev !== last.hash) {
+				return 'Not Mined. Previous hash is invalid';
+			}
+		} return 'Mined! Chain is Valid!';
+	}
+}
+
+let datum = new Protocol();
+console.log("mining... ");
+datum.add(
+	new Hash(1, "01/06/2020", {
+		sender: "Fiona Hacksworth",
+		recipient: "Rajan Agarwal",
+		quantity: 50
+	})
+);
+// hackercoin.protocol[1].data.quantity = 1000;
+console.log(datum.validity());
+console.log(JSON.stringify(datum, null, 5));
+
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import { useMoralis } from "react-moralis";
 import styles from "../styles/Home.module.css";
+import './api/hello.js';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await fetch('https://datum-api.rajnagrwl.repl.co/index.json');
+  const data = await res.json();
+
+  return { 
+      props: { graph: data }
+  }
+}
+
+export default function Home({ graph }) {
   const { authenticate, isAuthenticated, logout, user } = useMoralis();
 
   return (
@@ -19,6 +29,18 @@ export default function Home() {
           <button onClick={logout}>Logout</button>
           <h2>Welcome {user.get("username")}</h2>
           <h2>Your wallet address is {user.get("ethAddress")}</h2>
+          <div>
+            {graph.map(graphical => (
+                <div key={graphical.index}>
+                  <h3>Block: {graphical.index}</h3>
+                  Current Hash: {graphical.hash}<br/><br/>
+                  Previous Hash: {graphical.prev}<br/><br/>
+                  Keywords: {graphical.graph.keywords}<br/><br/>
+                  Validity: {graphical.graph.validity}<br/><br/>
+                  Single-Use Identifier: {graphical.nonce}<br/><br/>
+                </div>
+            ))}
+        </div>
         </>
       ) : ( 
         <button
